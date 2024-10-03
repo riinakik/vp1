143a8918f4eca44051c98db4262616996eaf97ac
Riina Kikkas <riinakik@users.noreply.github.com>
2024-10-03 15:39:09 +0300

    EJS view engine kasutusele võetud

--- index.js
+++ index.js
@@ -1,21 +1,74 @@
 const express = require("express");
 const dtEt = require("./dateTime");
+const fs = require("fs");
+//päringulahtiharutamiseks POST päringute puhul
+const bodyparser = require("body-parser");
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+//päringu urli parsimine, false, kui ainult tekst
+app.use(bodyparser.urlencoded({extended: false}));
 
-app.get("/", (req, res)=>{
+
+app.get("/vanasonad", (req, res)=>{
 	//res.send("Express läks käima!");
 	res.render("index.ejs");
 });
 
 app.get("/timenow",(req, res)=>{
-	const weekdayNow = dtEt.daysEt();
+	const weekdayNow = dtEt.dayEt();
 	const dateNow = dtEt.dateEt();
 	const timeNow = dtEt.hoursEt();
 	res.render("timenow", {nowWD: weekdayNow, nowD: dateNow, nowT: timeNow});
+
 	
 });
+
+app.get("/justlist", (req, res)=>{
+	let folkwisdom = [];
+	fs.readFile("public/textfiles/vanasonad.txt", "utf8", (err, data)=> {
+		if (err){
+			//throw err;
+			res.render("justlist", {h2: "Vanasõnad", listData: ["Ei leidnud ühtegi vanasõna!"]});
+		}
+		else{
+			folkwisdom = data.split(";");
+			res.render("justlist", {h2: "Vanasõnad", listData: folkwisdom});
+		}
+		
+	});
+	
+});
+
+app.get("/regvisit", (req, res)=>{
+	res.render ("regvisit");
+});
+
+app.post("/regvisit", (req, res)=>{
+	console.log(req.body);
+	fs.open("public/textfiles/visitlog.txt", "a", (err, file)=> {
+		if (err){
+			throw err;
+		}
+		else{
+			fs.appendFile("public/textfiles/visitlog.txt", req.body.firstNameInput + " " + req.body.lastNameInput + ";", (err)=> { 
+			 if (err){
+				 throw err;
+			 }
+			 else{
+				 console.log("Faili kirjutati!");
+				 res.render("regvisit");
+				 
+			 }
+			});
+			
+		}
+		
+	});
+	
+});
+
+
 
 app.listen(5113);
